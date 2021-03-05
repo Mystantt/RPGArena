69bec9aa9a34d10dacc84a54efc21d88daab9fce
Fabien DOMPEY <dompeydnf@gmail.com>
2021-03-05 16:36:09 +0100

    CommentInv

--- Assets/Scripts/InventorySystem/Inventory.cs
+++ Assets/Scripts/InventorySystem/Inventory.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class <c>Inventory</c> which is the implementation of a bag in the game, containing all sorts of items, restricted by a weight capacity
+/// Date : 05/03/2021
+/// Author : DOMPEY Fabien
+/// </summary>
 public class Inventory 
 {
     private List<Item> items;
@@ -15,12 +20,19 @@ public class Inventory
         WeightUsed = 0;
     }
 
+    /// <summary>
+    /// Add an item to the inventory
+    /// </summary>
+    /// <param name="i"> The item to add, its weight can't excess the capacity of the inventory (can't be null)</param>
     public void AddItem(Item i)
     {
-        if(WeightUsed + i.Weight <= WeightCapacity)
+        if(!i.Equals(null) && WeightUsed + i.Weight <= WeightCapacity)
         {
             WeightUsed += i.Weight;
             items.Add(i);
+        }else if (i.Equals(null))
+        {
+            throw new System.ArgumentNullException("The item can't be null");
         }
         else
         {
@@ -28,11 +40,19 @@ public class Inventory
         }
     }
 
+    /// <summary>
+    /// Remove an item from the inventory
+    /// </summary>
+    /// <param name="i"> The item to remove (can't be null)</param>
     public void RemoveItem(Item i)
     {
-        if (items.Contains(i))
+        if (!i.Equals(null) && items.Contains(i))
         {
             items.Remove(i);
+        }
+        else if (i.Equals(null))
+        {
+            throw new System.ArgumentNullException("The item can't be null");
         }
     }
 
