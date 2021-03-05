57afbb0a23a3361912223229495585579cbd681a
Fabien DOMPEY <dompeydnf@gmail.com>
2021-03-05 16:29:31 +0100

    Inventory

--- Assets/Scripts/InventorySystem/Inventory.cs
+++ Assets/Scripts/InventorySystem/Inventory.cs
@@ -6,10 +6,38 @@ public class Inventory
 {
     private List<Item> items;
     public int WeightCapacity { get; set; }
+    public int WeightUsed { get; private set; }
 
     public Inventory()
     {
         items = new List<Item>();
         WeightCapacity = 100;
+        WeightUsed = 0;
+    }
+
+    public void AddItem(Item i)
+    {
+        if(WeightUsed + i.Weight <= WeightCapacity)
+        {
+            WeightUsed += i.Weight;
+            items.Add(i);
+        }
+        else
+        {
+            throw new InventoryFullException("This item weighs too much to add to your inventory");
+        }
+    }
+
+    public void RemoveItem(Item i)
+    {
+        if (items.Contains(i))
+        {
+            items.Remove(i);
+        }
+    }
+
+    public List<Item> GetItems()
+    {
+        return new List<Item>(items);
     }
 }
---
+++ Assets/Scripts/InventorySystem/InventoryFullException.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryFullException : System.Exception
+{
+    public InventoryFullException(string s) : base(s) { }
+}
